32f1e060d98b770e6b0697e2018e98cbc4556f0a
Sergey Dankov <Dankov.SS@lexicom.ai>
2022-09-20 11:55:54 +0300

    Исправление загрузки файлов

--- app/Services/DataService/Asterisk.php
+++ app/Services/DataService/Asterisk.php
@@ -26,7 +26,7 @@ class Asterisk extends DataService
     {
         $db = app('db');
         $driver = new Driver($this->db);
-        $driver->setDriver('asterisk', 'mysql','asteriskcdrdb');
+        $driver->setDriver('asterisk-'.$this->db->getId(), 'mysql','asteriskcdrdb');
         $date = date('Y-m-d H:i:s', $this->getInstanceLastUpdate()->getTimestamp($this->db->getId()));
         /**
          * @var \Illuminate\Database\Query\Builder $items
