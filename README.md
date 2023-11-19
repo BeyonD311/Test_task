485f800af40e282f80253aecd11ecefb412001da
Sergey <dankovsergey31@outlook.com>
2023-11-19 22:18:45 +0300

    swagger and comment

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Swagger http://localhost:8071/
+
 Для запуска:
 * docker-compose up -d --build
 * docker-compose exec php bash
--- app/app/Http/Controllers/EventsController.php
+++ app/app/Http/Controllers/EventsController.php
@@ -2,15 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use App\Jobs\SendMessage;
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Redis;
 
 class EventsController extends Controller
 {
-    
-    public function send(Request $request) {
-
+    /**
+     * Принимает ассоциативный массив
+     * Описанный в validate
+     * @return JsonResponse
+     */
+    public function send(Request $request): JsonResponse 
+    {
+        
         $params = $this->validate($request, [
             "events" => "required|array|min:1",
             "events.*.account_id" => "required|int|max:1000"
